c120ce3f192dc93c5d5e64bab91bbde819294f05
DanielJDA <danieljda@DESKTOP-GVP3C2N.localdomain>
2021-05-26 10:05:56 -0600

    ssss

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,6 +7,9 @@ import axios from "axios";
 
 const spotifyApi = new spotifyWebApi({
   clientId: "fb9a97862c6740ce936275cbbbcc8551",
+  country: "SE",
+  locale: "sv_SE",
+  timestamp: "2014-10-23T09:00:00",
 });
 
 export default function Dashboard({ code }) {
@@ -23,7 +26,6 @@ export default function Dashboard({ code }) {
     setLyrics("");
   }
   useEffect(() => {
-    
     if (!playingTrack) return;
     axios
       .get("http://localhost:3001/lyrics", {
@@ -33,7 +35,6 @@ export default function Dashboard({ code }) {
         },
       })
       .then((res) => {
-        
         setLyrics(res.data.lyrics);
       });
   }, [playingTrack]);
@@ -43,13 +44,13 @@ export default function Dashboard({ code }) {
     spotifyApi.setAccessToken(accessToken);
   }, [accessToken]);
 
-  useEffect(() => {
+  /*useEffect(() => {
     if (!search) return setSearchResult([]);
     if (!accessToken) return;
 
     let cancel = false;
     spotifyApi.searchTracks(search).then((res) => {
-      console.log(res)
+      console.log(res);
       if (cancel) return;
       setSearchResult(
         res.body.tracks.items.map((track) => {
@@ -69,6 +70,36 @@ export default function Dashboard({ code }) {
         })
       );
     });
+    return () => (cancel = true);
+  }, [search, accessToken]);*/
+
+  useEffect(() => {
+    if (!search) return setSearchResult([]);
+    if (!accessToken) return;
+    let cancel = false;
+    spotifyApi
+      .getFeaturedPlaylists({
+        country: "SV",
+      })
+      .then((res) => {
+        console.log({ res });
+        if (cancel) return;
+        setSearchResult(
+          res.body.playlists.items.map((track) => {
+            /*const smallestAlbumImage = track.album.images.reduce(
+              (smallest, image) => {
+                if (image.height < smallest.height) return image;
+                return smallest;
+              },
+              track.album.images[0]
+            );*/
+            return {
+              title: track.name,
+              uri: track.uri,
+            };
+          })
+        );
+      });
     return () => (cancel = true);
   }, [search, accessToken]);
 
@@ -93,7 +124,9 @@ export default function Dashboard({ code }) {
             ))}
           </div>
           {searchResult.length === 0 && (
-            <div className="text-center flex justify-center items-center w-5/12 m-auto space-y-1.5 text-xl pt-8 px-14">{lyrics}</div>
+            <div className="text-center flex justify-center items-center w-5/12 m-auto space-y-1.5 text-xl pt-8 px-14">
+              {lyrics}
+            </div>
           )}
         </div>
       </div>
--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,10 @@
 import Button from "./button";
 export default function Login() {
   const AUTH_URL =
-    "https://accounts.spotify.com/authorize?client_id=fb9a97862c6740ce936275cbbbcc8551&response_type=code&redirect_uri=https://spotify-prueba-front.vercel.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+    "https://accounts.spotify.com/authorize?client_id=fb9a97862c6740ce936275cbbbcc8551&response_type=code&redirect_uri=https://spotify-prueba-front.vercel.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
   return (
-    <div className="w-full bg-image flex justify-center items-center h-screen ">
+    <div className="w-full bg-image flex justify-center items-center h-screen">
       <div className="px-4 py-4 bg-black fixed top-0 left-0 w-full">
         <Button children="ssss">
           <a className="px-3 py-1 text-white font-bold" href={AUTH_URL}>
@@ -17,8 +17,7 @@ export default function Login() {
           Escuchar <br /> es <br /> todo
         </p>
         <p className="mt-4">
-          Ingresa con la cuenta: <b>kiyts123</b> y contraseña:{" "}
-          <b>actinio_oracle</b>{" "}
+          Ingresa con la cuenta: <b></b> y contraseña: <b></b>{" "}
         </p>
       </div>
     </div>
